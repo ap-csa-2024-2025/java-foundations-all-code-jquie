75506649b5968de772b11b30346d686650723ded
jquie <jacquelynphan03@gmail.com>
2025-05-21 17:21:05 +0000

    done

--- src/jfo-section-3/Chickens01.java
+++ src/jfo-section-3/Chickens01.java
@@ -1,7 +1,19 @@
 public class Chickens01 {
     public static void main(String[] args) {
-        //Put yout code here
+        int totalEggs = 0;
+        int chickenCount = 3;
+        int eggsPerChicken = 5;
+
+        //Monday
+        totalEggs += chickenCount * eggsPerChicken;
         
+        //Tuesday
+        totalEggs += ++chickenCount * eggsPerChicken;
+
+        //Wednesday
+        chickenCount /= 2;
+        totalEggs += chickenCount * eggsPerChicken;
+
         System.out.println(totalEggs);
     }   
 }
