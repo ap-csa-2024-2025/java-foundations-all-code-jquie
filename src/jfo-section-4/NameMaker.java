ca75f3ef38e7a92191bd86f01dd99780e9811987
jquie <jacquelynphan03@gmail.com>
2025-05-23 17:15:06 +0000

    done

--- src/jfo-section-4/NameMaker.java
+++ src/jfo-section-4/NameMaker.java
@@ -1,9 +1,14 @@
-
+import javax.swing.JOptionPane;
+import java.util.Scanner;
 public class NameMaker {
     
     public static void main(String args[])
     {
-        
+        String name = JOptionPane.showInputDialog("Enter your name");
+        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
+        String address = JOptionPane.showInputDialog("Enter your address");
+        String message = String.format("Your name is %s%nYou are %d years old%nYou live at %s", name, age, address);
+        System.out.println(message);
     }
     
 }
